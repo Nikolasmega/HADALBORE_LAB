6bbfe7c767f624b05d09fa4a2e16502168a59ad1
Niko <niko@MacBook-Pro-Niko.local>
2026-06-28 17:27:26 +0300

    feat(db): add connection_type Threaded & Coupled to VAM 21

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME_PREFIX = 'hadalbore_lab-core-cache-';
-const BASE_VERSION = '1aa149b939dde6';
+const BASE_VERSION = 'e88e8677645d4';
 let activeCacheName = CACHE_NAME_PREFIX + BASE_VERSION;
 
 const ASSETS_TO_CACHE = [
