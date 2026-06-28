93adf61eddbd3e55b5c6d55a408f63585960808b
Niko <niko@MacBook-Pro-Niko.local>
2026-06-28 16:50:37 +0300

    docs: add database enrichment and localization report to knowledge_vault

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME_PREFIX = 'hadalbore_lab-core-cache-';
-const BASE_VERSION = '188ecd051c69b5';
+const BASE_VERSION = '285def5ebf460';
 let activeCacheName = CACHE_NAME_PREFIX + BASE_VERSION;
 
 const ASSETS_TO_CACHE = [
