5e1c288ad0c370e1fbb84e24541abc7f5bbef6f1
Niko <niko@MacBook-Pro-Niko.local>
2026-06-28 17:25:37 +0300

    feat(db): add connection_type Threaded & Coupled to Hydril Wedge 563

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME_PREFIX = 'hadalbore_lab-core-cache-';
-const BASE_VERSION = '285def5ebf460';
+const BASE_VERSION = '1aa149b939dde6';
 let activeCacheName = CACHE_NAME_PREFIX + BASE_VERSION;
 
 const ASSETS_TO_CACHE = [
