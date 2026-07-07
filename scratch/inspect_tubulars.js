f3443d7d84e2dacfeb47d2d812aadfeefffe9318
Niko <niko@MacBook-Pro-Niko.local>
2026-07-07 16:43:19 +0300

    build: ignore and untrack non-product logs, scratch scripts, plans and cache files

--- scratch/inspect_tubulars.js
+++
@@ -1,6 +0,0 @@
-import mockDb from '../src/data/mock-db.json' assert { type: 'json' };
-
-console.log('Current tubulars in DB:');
-mockDb.tubulars.forEach((t, i) => {
-  console.log(`${i + 1}. ID: ${t.id} | Name: ${t.name} | OD: ${t.od} | Grade: ${t.grade} | Type: ${t.tubular_type}`);
-});
