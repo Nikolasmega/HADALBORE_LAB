9ce255d93e4e968aceac918d51c8395c167421ed
Niko <niko@MacBook-Pro-Niko.local>
2026-07-12 21:18:12 +0400

    build: compile build version for precise sizes torque final

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME_PREFIX = 'hadalbore_lab-core-cache-';
-const BASE_VERSION = '169b66aa6dd245_ce8b159';
+const BASE_VERSION = '169b66aa6dd245_4c7cc84';
 let activeCacheName = CACHE_NAME_PREFIX + BASE_VERSION;
 
 const ASSETS_TO_CACHE = [
