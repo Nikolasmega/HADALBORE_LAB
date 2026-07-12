3feed2b6f79f0b7863ab30d65e75b609828ac3bf
Niko <niko@MacBook-Pro-Niko.local>
2026-07-12 11:10:59 +0400

    build: save sw.js and release_manifest.json with updated hash

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME_PREFIX = 'hadalbore_lab-core-cache-';
-const BASE_VERSION = '169b66aa6dd245_7f0418d';
+const BASE_VERSION = '169b66aa6dd245_8bb1e48';
 let activeCacheName = CACHE_NAME_PREFIX + BASE_VERSION;
 
 const ASSETS_TO_CACHE = [
