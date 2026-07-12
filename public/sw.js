fec1bf06c06752c7b33a312d815a78067b071142
Niko <niko@MacBook-Pro-Niko.local>
2026-07-12 11:05:48 +0400

    build: save updated sw.js with git hash in build version

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME_PREFIX = 'hadalbore_lab-core-cache-';
-const BASE_VERSION = '169b66aa6dd245';
+const BASE_VERSION = '169b66aa6dd245_5a77b8f';
 let activeCacheName = CACHE_NAME_PREFIX + BASE_VERSION;
 
 const ASSETS_TO_CACHE = [
