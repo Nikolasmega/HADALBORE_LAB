a4d9df612b8c66201701ed57857a1a51a666c420
Niko <niko@MacBook-Pro-Niko.local>
2026-07-12 11:21:39 +0400

    build: save compiled files with updated buildVersion for skill update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME_PREFIX = 'hadalbore_lab-core-cache-';
-const BASE_VERSION = '169b66aa6dd245_5f63f38';
+const BASE_VERSION = '169b66aa6dd245_084ad73';
 let activeCacheName = CACHE_NAME_PREFIX + BASE_VERSION;
 
 const ASSETS_TO_CACHE = [
